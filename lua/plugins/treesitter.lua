6a33927ecbd53bd939365545e83217be1152c733
Carlos L <clagccs@gmail.com>
2026-07-23 09:43:18 -0400

    fix: load nvim-treesitter eagerly to avoid missing first-buffer highlight

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    priority = 1000,
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, {
