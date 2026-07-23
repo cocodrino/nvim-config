7fb9b869cf3f95f564afe5c4cbb1c2ae052bc120
Carlos L <clagccs@gmail.com>
2026-07-23 09:56:34 -0400

    fix: add BufReadPost autocmd to catch empty filetype from Telescope race

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,15 @@
 require "nvchad.autocmds"
+
+-- Fix: Telescope first-open race where filetype is not detected
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function(args)
+    if vim.bo[args.buf].filetype == "" then
+      vim.defer_fn(function()
+        if vim.api.nvim_buf_is_valid(args.buf) and vim.bo[args.buf].filetype == "" then
+          vim.cmd "filetype detect"
+        end
+      end, 100)
+    end
+  end,
+})
+
